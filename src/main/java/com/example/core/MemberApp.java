704d7ae69ce2d711fdad158023177768b2850744
daeho kim <daeho.kim@softwareinlife.com>
2021-11-03 21:17:18 +0900

    first test case

---
+++ src/main/java/com/example/core/MemberApp.java
@@ -0,0 +1,20 @@
+package com.example.core;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new memberA = " + memberA + memberA.getName());
+        System.out.println("findMember = " + findMember + findMember.getName());
+    }
+}
---
+++ src/main/java/com/example/core/member/Grade.java
@@ -0,0 +1,5 @@
+package com.example.core.member;
+
+public enum Grade {
+    BASIC, VIP
+}
