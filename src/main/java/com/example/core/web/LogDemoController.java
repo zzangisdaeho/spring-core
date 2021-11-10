f3b0cd78108ee7b0282a4dfb4f92b7e9b2249e40
DaeHo Kim <zzangisdaeho@naver.com>
2021-11-11 03:31:00 +0900

    spring core - basic fin

---
+++ src/main/java/com/example/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+//        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.setRequestURL(request.getRequestURL().toString());
+
+        myLogger.log("controller test");
+
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
